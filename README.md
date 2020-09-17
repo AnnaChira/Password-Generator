115eb4f99e16edb16cb1185f4475b96aa9b3f5e4
Anna Chirapongse <achirapongse@gmail.com>
2020-09-16 22:51:18 -0400

    Last updates

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 //Next is updating the create the password after the users had been prompted.
 //I have created the new string to create the password when the user is prompted.
 // Updates to the script and trying to make it return based on the user selections.
+// The last updates for the password generator.
--- script.js
+++ script.js
@@ -6,67 +6,61 @@ var passwordLength;
 var upperCase;
 var lowerCase;
 
-  //var password = " ";
 upperCase = "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
 lowerCase = "abcedefghijklmnopqrstuvwxyz";
 specialCharacters = "!@#$%&*()^<>?";
 numbers = "123456789";
-    //return password;
-
 
 function generatePassword() {
     
-    if (!numbers && !specialCharacters && !upperCase && !lowerCase){
-        valid = specialCharacters.concat(numbers, upperCase, lowerCase);
+    if (numbers && specialCharacters && upperCase && lowerCase){
+        confirm = numbers.concat(specialCharacters, upperCase, lowerCase);
     }
-    else if (!numbers && !specialCharacters && !upperCase){
-        valid = specialCharacters.concat(numbers, upperCase);
+    else if (numbers && specialCharacters && upperCase){
+        confirm = numbers.concat(specialCharacters, upperCase);
     }
-    else if (!numbers && !specialCharacters && !lowerCase){
-        valid = specialCharacters.concat(numbers, lowerCase);
+    else if (numbers && specialCharacters && lowerCase){
+        confirm = numbers.concat(specialCharacters, lowerCase);
     }
-    else if (!numbers && !lowerCase){
-        valid = numbers.concat(lowerCase);
+    else if (numbers && lowerCase){
+        confirm = numbers.concat(lowerCase);
     }
-    else if (!numbers && !upperCase){
-        valid = numbers.concat(upperCase);
+    else if (numbers && upperCase){
+        confirm = numbers.concat(upperCase);
     }
-    else if (!specialCharacters && upperCase){
-        valid = specialCharacters.concat(upperCase);
+    else if (specialCharacters && upperCase){
+        confirm = specialCharacters.concat(upperCase);
     }
-    else if (!specialCharacters && !!numbers){
-        valid = specialCharacters.concat(numbers);
+    else if (specialCharacters && numbers){
+        confirm = specialCharacters.concat(numbers);
     }
-    else if (!specialCharacters && lowerCase){
-        valid = specialCharacters.concat(lowerCase);
+    else if (specialCharacters && lowerCase){
+        confirm = specialCharacters.concat(lowerCase);
     }
-    else if (!upperCase && !!lowerCase){
-        valid = upperCase.concat(lowerCase);
+    else if (upperCase && lowerCase){
+        confirm = upperCase.concat(lowerCase);
     }
     else if (specialCharacters){
-        valid = specialCharacters;
+        confirm = specialCharacters;
     }
     else if (numbers){
-        valid = numbers;
+        confirm = numbers;
     }
     else if (upperCase){
-        valid = upperCase;
+        confirm = upperCase;
     }
     else if (lowerCase){
-        valid = lowerCase;
+        confirm = lowerCase;
     }
     if (passwordLength!==''){
         for (var i=0; i < passwordLength.value; i++){
             text += shuffle.charAt(Math.floor.random() * shuffle.length);
         }
     }
-    var ps = password.concat("");
-    UserInput(ps);
-    return generatePassword;
+    return password;
    
 }
     
-
 function validate(){
     if (!numbers && !specialCharacters && !upperCase && !lowerCase){
         alert("You must chose at least one!");
@@ -83,7 +77,6 @@ function validate(){
     }
 }
 
-// Write password to the #password input
 function writePassword() {
     numbers = confirm ("Do you want numbers?");
     specialCharacters = confirm ("Do you want special charatcers?");
@@ -103,5 +96,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
